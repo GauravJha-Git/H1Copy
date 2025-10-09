45d5f81043b4e2deb5590d9621e99b3a04940ee4
GauravJha-Git <gauravjha092006@gmail.com>
2025-10-09 23:45:42 +0530

    some more small changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -170,8 +170,9 @@ export function Services({ onNavigate }: ServicesProps) {
         >
           <div className="text-center mb-12">
             <h3 className="text-3xl font-bold text-gray-900 mb-4">
-              Our predictable content workflow
-            </h3> 
+              Get product-led content
+            </h3>
+            
           </div>
 
           
